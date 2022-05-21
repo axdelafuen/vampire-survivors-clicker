d5c632261a56600c0e121fa4695dac625c00a584
axdelafuen <axel.dlf@free.fr>
2022-05-21 16:39:00 +0200

    creation du menu + mise en place amélioration + help page

--- script/clicker.js
+++ script/clicker.js
@@ -1,6 +1,31 @@
 var clicks = 0;
+var multipl = 1;
+var costs = 200;
+
+counter.innerHTML = clicks;
+multipli.innerHTML = multipl*2;
+cost.innerHTML = costs;
 
 function increment(){
-    clicks = clicks + 1;
+    clicks = clicks + multipl;
     counter.innerHTML = clicks;
+}
+
+function dispMenu(menu,style){
+    document.querySelector(menu).style.display = style;
+}
+
+function clickMulti(){
+    if(clicks >= costs){
+        clicks = clicks - multipl*200;
+        counter.innerHTML = clicks;
+        multipl = multipl*2;
+        costs = costs * multipl;
+    }
+    else{
+        alert("You don't have enough clicks ! ");
+        return;
+    }
+    cost.innerHTML = costs;
+    multipli.innerHTML = multipl*2;
 }
